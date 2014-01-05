d5a2f4e62a9a6f00adcf0dea5acadde954e2170e
ygneo <antonio.barcia@gmail.com>
2014-01-05 21:29:58 +0100

    Handle registration complete URL using a TemplateView pointing to registration/registration_complete.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.views import logout
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = patterns('',
                        url(r'^logout/$', logout),
+                       url(r'^register/complete/$',
+                           TemplateView.as_view(template_name='registration/registration_complete.html'),
+                           name='registration_complete'),
+
 )
