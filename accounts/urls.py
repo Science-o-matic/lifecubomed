af6d0721ca5c2f1e7134601f3373e5fbcbb87754
ygneo <antonio.barcia@gmail.com>
2014-01-05 22:03:23 +0100

    Handle activate and activation complete URLs, and add corresponding templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.views import logout
 from django.views.generic.base import TemplateView
+from registration.backends.default.views import ActivationView
 
 
 urlpatterns = patterns('',
@@ -8,5 +9,11 @@ urlpatterns = patterns('',
                        url(r'^register/complete/$',
                            TemplateView.as_view(template_name='registration/registration_complete.html'),
                            name='registration_complete'),
+                       url(r'^activate/complete/$',
+                           TemplateView.as_view(template_name='registration/activation_complete.html'),
+                           name='registration_activation_complete'),
+                       url(r'^activate/(?P<activation_key>\w+)/$',
+                           ActivationView.as_view(),
+                           name='registration_activate'),
 
 )
