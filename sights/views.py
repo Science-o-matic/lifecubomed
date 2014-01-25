88f60f65448dce3c05424efaec77cd8cf3a6f8a7
ygneo <antonio.barcia@gmail.com>
2014-01-25 21:07:20 +0100

    Add form_valid method to SightReportView, adding reporter_id and saving sight

--- sights/views.py
+++ sights/views.py
@@ -6,3 +6,9 @@ class SightReportView(FormView):
     template_name = 'sight_report_form.html'
     form_class = SightReportForm
     success_url = '/'
+
+    def form_valid(self, form):
+        sight = form.save(commit=False)
+        sight.reporter_id = self.request.user.id
+        sight.save()
+        return super(SightReportView, self).form_valid(form)
