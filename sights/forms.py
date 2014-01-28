70181b2b0d385b8d0745daafd1a4707d96e43cd4
ygneo <antonio.barcia@gmail.com>
2014-01-28 19:42:34 +0100

    Rendering and style of jellyfishes images in report sight form. Also, add required class to form and corresponding css.

--- sights/forms.py
+++ sights/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from sights.models import Sight, Jellyfish
 
 
 class SightReportForm(forms.ModelForm):
+    required_css_class = 'required'
 
     class Meta:
         model = Sight
--- sights/views.py
+++ sights/views.py
@@ -1,11 +1,17 @@
 from sights.forms import SightReportForm
 from django.views.generic.edit import FormView
+from sights.models import Jellyfish
 
 
 class SightReportView(FormView):
     template_name = 'sight_report_form.html'
     form_class = SightReportForm
     success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super(SightReportView, self).get_context_data(**kwargs)
+        context['jellyfishes'] = Jellyfish.objects.all()
+        return context
 
     def form_valid(self, form):
         sight = form.save(commit=False)
