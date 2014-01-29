29e3a358de37a69e3369712b0e2082c79aaba744
ygneo <antonio.barcia@gmail.com>
2014-01-29 19:47:18 +0100

    Show/hide google map.

--- static/js/sight_report_form.js
+++ static/js/sight_report_form.js
@@ -1,42 +1,90 @@
-$(document).ready(function () {
+function Map() {
+  this.gmap_id = 'gmap';
+  this.map = null;
+  this.geocoder = null;
 
-  jQuery.fn.center = function () {
-    var t = this.parent().position().top - 4;
-    var l = this.parent().position().left;
-    var parent_pl = parseInt(this.parent().css("padding-left"));
-    var parent_pr = parseInt(this.parent().css("padding-right"));
-    var parent_width = this.parent().width() + parent_pl + parent_pr;
-    var pl = parseInt(this.css("padding-left"));
-    var pr = parseInt(this.css("padding-right"));
-    var width = this.width() + pl + pr;
+  this.load = function() {
+    if (GBrowserIsCompatible()) {
+      this.map = new GMap2(document.getElementById(this.gmap_id));
+      this.map.setCenter(new GLatLng(38.84031375122825,0.11510465294122696),10);
+      this.map.setMapType(G_SATELLITE_MAP);
+      this.geocoder = new GClientGeocoder();
 
-    this.css("position","absolute");
-    this.css("top", ((this.parent().height() - this.outerHeight()) / 2) + t + "px");
-    this.css("left", ((parent_width - width) / 2) + l + parent_pl + pl + "px");
-    return this;
-  }
-
-  jQuery.fn.handleJellyFishImageClick = function () {
-    if ($("input[name=specimen_type]:checked").val() == "other") {
-      $("#known_specimen_type").prop("checked", true);
-      $("#other_jellyfish_description").hide();
+      GEvent.addListener(
+        this.map, "click",
+        function(marker, point) {
+          if (marker) {
+            null;
+          } else {
+            map.clearOverlays();
+            var marcador = new GMarker(point);
+            map.addOverlay(marcador);
+            document.form_mapa.coordenadas.value = point.y+","+point.x;
+          }
+        }
+      );
     }
-    $("." + $(this).attr("class")).removeClass("selected");
-    $(this).addClass("selected");
+  };
 
-    id = $(this).attr("data-id");
-    $("input[name=jellyfish]").val(id);
+  this.showAddress = function(address, zoom) {
+    if (this.geocoder) {
+      this.geocoder.getLatLng(
+        address,
+        function(point) {
+          if (!point) {
+            alert(address + " not found");
+          } else {
+            map.setCenter(point, zoom);
+            var marker = new GMarker(point);
+            map.addOverlay(marker);
+            //marker.openInfoWindowHtml("<b>"+address+"</b><br>Coordenadas:<br>Latitud : "+point.y+"<br>Longitud : "+point.x+"<a href=http://www.mundivideo.com/fotos_pano.htm?lat="+point.y+"&lon="+point.x+"&mapa=3 TARGET=fijo><br><br>Fotografias</a>");
+            // marker.openInfoWindowHtml("<b>Coordenadas:</b> "+point.y+","+point.x);
+            document.form_mapa.coordenadas.value = point.y+","+point.x;
+          }
+        }
+      );
+    }
+  };
+}
 
-    info = $("#jellyfish_info");
-    $(this).append(info);
-    info.hide();
-    info.show();
-    info.center();
+jQuery.fn.center = function () {
+  var t = this.parent().position().top - 4;
+  var l = this.parent().position().left;
+  var parent_pl = parseInt(this.parent().css("padding-left"));
+  var parent_pr = parseInt(this.parent().css("padding-right"));
+  var parent_width = this.parent().width() + parent_pl + parent_pr;
+  var pl = parseInt(this.css("padding-left"));
+  var pr = parseInt(this.css("padding-right"));
+  var width = this.width() + pl + pr;
+
+  this.css("position","absolute");
+  this.css("top", ((this.parent().height() - this.outerHeight()) / 2) + t + "px");
+  this.css("left", ((parent_width - width) / 2) + l + parent_pl + pl + "px");
+  return this;
+}
+
+jQuery.fn.handleJellyFishImageClick = function () {
+  if ($("input[name=specimen_type]:checked").val() == "other") {
+    $("#known_specimen_type").prop("checked", true);
+    $("#other_jellyfish_description").hide();
   }
+  $("." + $(this).attr("class")).removeClass("selected");
+  $(this).addClass("selected");
+
+  id = $(this).attr("data-id");
+  $("input[name=jellyfish]").val(id);
+
+  info = $("#jellyfish_info");
+  $(this).append(info);
+  info.hide();
+  info.show();
+  info.center();
+}
+
+$(document).ready(function () {
 
   $('input[name=date]').datepicker();
   $(".jellyfish_image.selected").handleJellyFishImageClick();
-
 
   $(".jellyfish_image").click(function () {
     $(this).handleJellyFishImageClick();
@@ -62,6 +110,12 @@ $(document).ready(function () {
       $("#open_gmap img").attr("src", $("#open_gmap img").attr("data-hide-src"));
     } else {
       $("#open_gmap img").attr("src", $("#open_gmap img").attr("data-show-src"));
+    }
+    if ($("#gmap").is(":visible")) {
+      map = new Map();
+      map.load();
+    } else {
+      GUnload();
     }
   });
 
