e65a69fa54fc81bd8870742a158cf3f2db3bc319
ygneo <antonio.barcia@gmail.com>
2014-02-04 21:42:57 +0100

    Implement custom serialiazer for Sighting objects and use it in AJAXSightingsListView

--- sightings/views.py
+++ sightings/views.py
@@ -1,6 +1,6 @@
+from django.core.serializers.json import Serializer
 from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse
-from django.core import serializers
 from django import http
 from django.views.generic.list import ListView
 from sightings.models import Jellyfish, Sighting
@@ -23,33 +23,46 @@ class SightingReportView(FormView):
         return http.HttpResponseRedirect(form.data["next"])
 
 
+class FlatSightingSerializer(Serializer):
+
+    def get_dump_object(self, obj):
+        self._current.update({
+                'id': obj._get_pk_val(),
+                'date': obj.date.isoformat(),
+                'jellyfish': {
+                    'name': unicode(obj.jellyfish) or _("N/A"),
+                    'id': obj.jellyfish_id
+                },
+                'reporter': {
+                    'name': unicode(obj.reporter),
+                    'id': obj.reporter.id
+                },
+        })
+        return self._current
+
 class AJAXListMixin(object):
 
      def dispatch(self, request, *args, **kwargs):
          if not request.is_ajax():
-             import ipdb; ipdb.set_trace()
-
-
              raise http.Http404("This is an ajax view.")
          return super(AJAXListMixin, self).dispatch(request, *args, **kwargs)
 
-     def get_queryset(self):
-         return (
-            super(AJAXListMixin, self)
-            .get_queryset()
-          )
-          #  .filter(ajaxy_param=self.request.GET.get('some_ajaxy_param'))
-
-
      def get(self, request, *args, **kwargs):
-         return http.HttpResponse(
-             serializers.serialize(
-                 'json',
-                 self.get_queryset(),
-                 indent=2,
-             )
-         )
+         serializer = FlatSightingSerializer()
+         json = serializer.serialize(self.get_queryset(),
+                              indent=2,
+                              fields=('pk', 'date', 'lat', 'lng', 'reporter',
+                                      'jellyfish', 'jellyfish_quantity',
+                                      'jellyfish_size')
+                              )
+         return http.HttpResponse(json)
+
 
 
 class AJAXSightingsListView(AJAXListMixin, ListView):
      model = Sighting
+
+     def get_queryset(self):
+         qs = super(AJAXSightingsListView, self).get_queryset()
+         qs.select_related("jellyfish", "reporter")
+         return qs
